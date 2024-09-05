76914209bd914a6966e1b4da8bf34c6b8185fc28
DanielHutabarat <2381015@unai.edu>
2024-09-05 11:45:04 +0700

    handson 2

--- latihan1/latihan3.java
+++ latihan1/latihan3.java
@@ -5,46 +5,48 @@ import java.util.Scanner;
 public class latihan3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int kalkulator [] = new int[2];
 
-        System.out.print("Masukan Bilangan Pertama: ");
-        int num1 = input.nextInt();
+        System.out.print("Masukan Bilangan Pertama:");
+        kalkulator[0] = input.nextInt();
 
-        System.out.print("Masukan Bilangan Kedua: ");
-        int num2 = input.nextInt();
-
-        System.out.print("Pilih Operator (+, -, x, /, %): ");
+        System.out.print("Pilih Operator (+, -, x, /, %,): ");
         char operator = input.next().charAt(0);
+
+        System.out.print("Masukan Bilangan Kedua:");
+        kalkulator[1] = input.nextInt();
+        
 
         int result = 0;
 
-        switch (operator) {
+        switch (operator){
             case '+':
-                result = num1 + num2;
+                result = kalkulator[0] + kalkulator[1];
                 break;
             case '-':
-                result = num1 - num2;
+                result = kalkulator[0] - kalkulator[1];
                 break;
             case 'x':
-                result = num1 * num2;
+                result = kalkulator[0] * kalkulator[1];
                 break;
             case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error: Division by zero!");
-                    return;
-                }
+                if (kalkulator[1] != 0){
+                    result = kalkulator[0] / kalkulator[1];
+            } else {
+                System.out.println("Error: Pembagian Menggunakan 0");
+                return;
+            }
                 break;
             case '%':
-                if (num2 != 0) {
-                    result = num1 % num2;
-                } else {
-                    System.out.println("Error: Division by zero!");
+                if (kalkulator[1] != 0){
+                    result = kalkulator[0] % kalkulator[1];
+                }else {
+                    System.out.println("Error: Pembagian Habis Menggunakan 0");
                     return;
                 }
                 break;
             default:
-                System.out.println("Error: Invalid operator!");
+                System.out.println("ERROR!!");
                 return;
         }
 
